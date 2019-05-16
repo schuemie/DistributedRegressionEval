b9939fed3be95808785e276ecab12aececa2f63b
schuemie <schuemie@ohdsi.org>
2019-05-16 10:05:34 -0400

    Using multiple local DBs for multivar Cox

--- R/EvaluateMultiVarCox.R
+++ R/EvaluateMultiVarCox.R
@@ -1,6 +1,6 @@
 evaluateCox <- function(studyFolder, outcomeId) {
   writeLines(paste("Evaluating outcome", outcomeId))
-  # outcomeId <- 5
+  # outcomeId <- 3 # 3 = stroke
   data <- readRDS(file.path(studyFolder, sprintf("data_o%s.rds", outcomeId)))
   data$age_in_years <- NULL
   data$`gender_=_FEMALE` <- NULL
@@ -53,8 +53,9 @@ evaluateCox <- function(studyFolder, outcomeId) {
   pooledCox <- survival::coxph(Surv(dataCombined$all_data$t_surv, dataCombined$all_data$ind_event) ~ dataCombined$all_data$X)
 
   ## try using each Db as Local and run DistCox
-  # for(localDb in Db.names){
-  localDb <- "ccae"
+  Big.Db.names <- c('ccae',	'mdcd',	'optum',	'mdcr')
+  for(localDb in Big.Db.names){
+  # localDb <- "ccae"
     cat(localDb, '...\n')
     otherDbs <- unique(data$database[data$database != localDb])
 
@@ -87,91 +88,27 @@ evaluateCox <- function(studyFolder, outcomeId) {
                                      init_est = beta_avgCox)
     assign(paste0('distCoxAvgInit.', localDb), distCoxAvgInit)
 
-  # }
+  }
 
 
 
 
   # comparison: DistCox (beta_tilde) obtains coef est better than using local only, closer to use pooled data
-  # results <- data.frame(description = c("Local beta",
-  #                                       "ODACO local init beta_tilde",
-  #                                       "ODACO local init beta_N",
-  #                                       "ODACO average init beta_tilde",
-  #                                       "ODACO average init beta_N",
-  #                                       "pooled beta"),
-  #                       beta = c(localCox$par,
-  #                                distCoxLocalInit$beta_tilde,
-  #                                distCoxLocalInit$beta_N,
-  #                                distCoxAvgInit$beta_tilde,
-  #                                distCoxAvgInit$beta_N,
-  #                                pooledCox$coef))
-
-  tmp <- rbind(get(paste0('localCox.', 'ccae'))$par,
-               c(beta_avgCox),
-  c(distCoxLocalInit$beta_tilde),
-  c(distCoxAvgInit$beta_tilde),
-  c(distCoxLocalInit$beta_tilde2),
-  c(distCoxAvgInit$beta_tilde2),
-  # distCoxAvgInit.ccae$beta_N,
-  pooledCox$coef)
-  row.names(tmp) <- c("Local beta",
-                      "Average beta",
-                      "ODACO local init beta_tilde",
-                      "ODACO average init beta_tilde",
-                      "ODACO2 local init beta_tilde",
-                      "ODACO2 average init beta_tilde",
-                      # "pooled beta_N",
-                      "pooled beta_pkg")
-  write.csv(tmp, file.path(studyFolder, "output.csv"))
-
-  return(NULL)
-  # Stopping here ------------------------
-
-  results <- data.frame(description = c("Local beta",
-                                        "Average beta",
-                                        "ODACO local init beta_tilde",
-                                        "ODACO average init beta_tilde",
-                                        "ODACO2 local init beta_tilde",
-                                        "ODACO2 average init beta_tilde",
-                                        # "pooled beta_N",
-                                        "pooled beta_pkg"),
-                        tmp)
-
-
-  write.csv(results, file.path(studyFolder, "results.csv"), row.names = FALSE)
-
-  # compare s.d. estimates
-  # cbind(distCox$sigma_tilde,
-  #       summary(pooledCox)$coef[,3] )
-  Cox.sd <- data.frame(description = c("Local sd",
-                                       "ODACO local init sd_tilde",
-                                       "ODACO average init sd_tilde",
-                                       "pooled sd_N",
-                                       "pooled sd_pkg"),
-                       ccae = c(sqrt(diag(solve(localCox.ccae$hessian))/ n.ccae),
-                                distCoxLocalInit.ccae$sigma_tilde,
-                                distCoxAvgInit.ccae$sigma_tilde,
-                                sqrt(diag(solve(distCoxAvgInit.ccae$sol_N$hessian))/ n.total),
-                                summary(pooledCox)$coef[,3]),
-                       Jmdc = c(sqrt(diag(solve(localCox.Jmdc$hessian))/ n.Jmdc),
-                                distCoxLocalInit.Jmdc$sigma_tilde,
-                                distCoxAvgInit.Jmdc$sigma_tilde,
-                                sqrt(diag(solve(distCoxAvgInit.Jmdc$sol_N$hessian))/ n.total),
-                                summary(pooledCox)$coef[,3]),
-                       mdcd = c(sqrt(diag(solve(localCox.mdcd$hessian))/ n.mdcd),
-                                distCoxLocalInit.mdcd$sigma_tilde,
-                                distCoxAvgInit.mdcd$sigma_tilde,
-                                sqrt(diag(solve(distCoxAvgInit.mdcd$sol_N$hessian))/ n.total),
-                                summary(pooledCox)$coef[,3]),
-                       optum = c(sqrt(diag(solve(localCox.optum$hessian))/ n.optum),
-                                 distCoxLocalInit.optum$sigma_tilde,
-                                 distCoxAvgInit.optum$sigma_tilde,
-                                 sqrt(diag(solve(distCoxAvgInit.optum$sol_N$hessian))/ n.total),
-                                 summary(pooledCox)$coef[,3]),
-                       panther = c(sqrt(diag(solve(localCox.panther$hessian))/ n.panther),
-                                   distCoxLocalInit.panther$sigma_tilde,
-                                   distCoxAvgInit.panther$sigma_tilde,
-                                   sqrt(diag(solve(distCoxAvgInit.panther$sol_N$hessian))/ n.total),
-                                   summary(pooledCox)$coef[,3]))
-  write.csv(Cox.sd, file.path(studyFolder, "sd-results.csv"), row.names = FALSE)
+  for (localDb in Big.Db.names){
+    results <- rbind(c(get(paste0("localCox.", localDb))$par),
+                     c(beta_avgCox),
+                     c(get(paste0("distCoxLocalInit.", localDb))$beta_tilde),
+                     c(get(paste0("distCoxAvgInit.", localDb))$beta_tilde),
+                     c(get(paste0("distCoxLocalInit.", localDb))$beta_tilde2),
+                     c(get(paste0("distCoxAvgInit.", localDb))$beta_tilde2),
+                     c(pooledCox$coef))
+    row.names(results) <- c("Local beta",
+                            "Average beta",
+                            "ODACO local init beta_tilde",
+                            "ODACO average init beta_tilde",
+                            "ODACO2 local init beta_tilde",
+                            "ODACO2 average init beta_tilde",
+                            "pooled beta_pkg")
+    write.csv(results, file.path(studyFolder, sprintf("output_%s.csv", localDb)))
+  }
 }
